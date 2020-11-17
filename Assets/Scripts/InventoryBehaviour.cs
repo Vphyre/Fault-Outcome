24e76abf106048e135e853c74ace045220beb3b5
Vphyre <vinipaulnic@hotmail.com>
2020-11-17 11:56:50 -0300

    introducing logbook (in progress)

---
+++ Assets/Scripts/InventoryBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryBehaviour : MonoBehaviour
+{
+    public static bool logBook;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        logBook = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+    }
+}
---
+++ Assets/Scripts/LogBookBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogBookBehaviour : MonoBehaviour
+{
+    public static bool canUse;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OpenClose();
+    }
+    public void OpenClose()
+    {
+        if(InventoryBehaviour.logBook)
+        {
+            if(Input.GetKey(KeyCode.L))
+            {
+                print("Abriu log book");
+            }
+        }
+
+    }
+}
